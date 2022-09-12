0796d36f21c6ca75ddd80ed4afc2e5f447f5737a
Jorge Tadeu <jsbatalha@gmail.com>
2022-09-12 16:59:39 -0300

    Formating schema

--- src/Schemas/wifiSchemas.ts
+++ src/Schemas/wifiSchemas.ts
@@ -4,5 +4,5 @@ import Joi from "joi";
 export const wifiSchema = Joi.object<Wifi>({
     title: Joi.string().required(),
     name: Joi.string().required(),
-    password: Joi.string().required(),
+    password: Joi.string().required()
 });
